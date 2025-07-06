59e7718748f89f87e8f302aba165d33b02ec076c
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-06 11:03:50 +0300

    home repo

---
+++ lib/Features/home/domain/entities/repos/home_repo.dart
@@ -0,0 +1,6 @@
+import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRepo {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}

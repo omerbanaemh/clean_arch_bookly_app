c1cee75a8b19af77c1d657eea347cf36dd77fc28
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-13 15:42:51 +0300

    integrate featured books cubit

--- lib/Features/home/presentation/manger/featured_books_cubit/cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/cubit/featured_books_cubit.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
 import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch_bookly_app/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';

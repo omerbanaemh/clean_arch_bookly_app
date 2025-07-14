7ea9f7971631ba55e4ddb2f6bfe593b7366b590d
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-14 11:23:42 +0300

    create: similar books use case

---
+++ lib/Features/home/domain/use_cases/fetch_similar_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_arch_bookly_app/Features/home/domain/repos/home_repo.dart';
+import 'package:clean_arch_bookly_app/core/errors/failure.dart';
+import 'package:clean_arch_bookly_app/core/use_cases/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchSimilarBooksUseCase extends UseCase<List<BookEntity>, int>{
+  final HomeRepo homeRepo;
+
+  FetchSimilarBooksUseCase({required this.homeRepo});
+  @override
+  Future<Either<Failure,  List<BookEntity>>> call([int pageNumber = 0]) async {
+    return await homeRepo.fetchSimilarBooks(pageNumber: pageNumber);
+  }
+}

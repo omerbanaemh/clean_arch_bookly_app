4d16b54f2385d35d9e8ed58af3aaed9fbd98d30a
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-14 11:51:31 +0300

    create: Similar-books Local-Data_Source

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -4,7 +4,9 @@ import 'package:hive/hive.dart';
 
 abstract class HomeLocalDataSource {
   List<BookEntity> fetchFeaturedBooks({int pageNumber = 0});
-  List<BookEntity> fetchNewestBooks();
+  List<BookEntity> fetchNewestBooks({int pageNumber = 0});
+  List<BookEntity> fetchSimilarBooks({int pageNumber = 0});
+
 }
 
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
@@ -25,6 +27,18 @@ class HomeLocalDataSourceImpl extends HomeLocalDataSource {
     int startIndex = pageNumber * 10;
     int endIndex = (pageNumber + 1) * 10; 
     var box = Hive.box<BookEntity>(kNewestBox);
+    int length = box.values.length;
+    if (startIndex >= length || endIndex > length) {
+      return [];
+    }
+    return box.values.toList();
+  }
+  
+  @override
+  List<BookEntity> fetchSimilarBooks({int pageNumber = 0}) {
+    int startIndex = pageNumber * 10;
+    int endIndex = (pageNumber + 1) * 10; 
+    var box = Hive.box<BookEntity>(kSimilarBox);
     int length = box.values.length;
     if (startIndex >= length || endIndex > length) {
       return [];

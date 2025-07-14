af7580fb4a0d65930aa9838c9d84867631753520
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-14 10:45:18 +0300

    display book in details page

--- lib/Features/home/presentation/views/book_details_view.dart
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -1,11 +1,13 @@
+import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch_bookly_app/Features/home/presentation/views/widgets/book_details_view_body.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsView extends StatelessWidget {
-  const BookDetailsView({super.key});
+  const BookDetailsView({super.key, required this.book});
 
+final BookEntity book;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: BookDetailsViewBody()));
+    return Scaffold(body: SafeArea(child: BookDetailsViewBody(book: book,)));
   }
 }
